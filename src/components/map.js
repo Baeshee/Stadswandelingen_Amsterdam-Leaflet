0f315d01ca182a313eab5e3988f16a64e7c53e33
Bas Barg <75019040+Baeshee@users.noreply.github.com>
2022-04-13 15:30:48 +0200

    Update map.js

--- src/components/map.js
+++ src/components/map.js
@@ -73,8 +73,8 @@ class Map extends Component {
             <MapContainer center={[this.props.crntLoc.lat, this.props.crntLoc.lng]} zoom={this.props.crntLoc.zoom} zoomControl={false} scrollWheelZoom={this.props.crntLoc.scrollWheel} style={{ width: '100%', height: '400px' }}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url={!this.props.dm ? "https://api.mapbox.com/styles/v1/baeshee/cl0s994gl00e314jzwp9u4odl/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYmFlc2hlZSIsImEiOiJjbDByejBrMmgwODJ6M2NxbzZnbHFtN2ppIn0.ji3kfWiNhBrq0PHfK2saTA" :
-                                                "https://api.mapbox.com/styles/v1/baeshee/cl0s950qk000115nqmr6t6g69/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYmFlc2hlZSIsImEiOiJjbDByejBrMmgwODJ6M2NxbzZnbHFtN2ppIn0.ji3kfWiNhBrq0PHfK2saTA"
+                    url={!this.props.dm ? "{Mapbox Tile Url}" :
+                                                "{Mapbox Tile Url}"
                         }
                 />
 
